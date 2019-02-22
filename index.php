2fabdd85cf8a34c8b9247a3f27ffc319e6b08652
nds <griganed@gmail.com>
2019-02-22 10:55:55 +0200

    wip page as index.php

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<?php require_once 'bootstrap.php'; ?>
-<html>
-    <head>
-        <title>PZ2ABOOK | Welcome!</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <!-- Navigation -->    
-        <?php require 'objects/navigation.php'; ?>        
-
-        <!-- Content -->   
-        <h1>Welcome to PZDABOOK!</h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/wip.css">
+  <title>Enjoy</title>
+</head>
+<body>
+  <div class="header">
+    <h3>Cummmming SOOOOOON!!!!!!</h3>
+    <p>Inspired by zirgas69</p>
+  </div>
+  <div class="flex">
+    <div class="box1"></div>
+    <div class="box2"></div>
+  </div>
+  <div class="box3"></div>
+</body>
 </html>
