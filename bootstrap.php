b44ca5663088c7d7d9482956e13fa4045a0617b5
nds <griganed@gmail.com>
2019-02-22 10:25:51 +0200

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//define('STORAGE_FILE', 'files/teams.txt');
+//define('USERS_FILE', 'files/players.txt');
+
+require_once 'functions/form.php';
+require_once 'functions/file.php';
+require_once 'functions/app.php';
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once 'bootstrap.php'; ?>
+<html>
+    <head>
+        <title>PZ2ABOOK | Welcome!</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!-- Navigation -->    
+        <?php require 'objects/navigation.php'; ?>        
+
+        <!-- Content -->   
+        <h1>Welcome to PZDABOOK!</h1>
+    </body>
+</html>
